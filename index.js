6241c871e7fbbddeafe770cd685e1ec494d4c29b
mesum357 <i.mesumabbas@gmail.com>
2025-04-25 03:03:46 +0500

    Last One

--- index.js
+++ index.js
@@ -1,61 +1,84 @@
-// JS
-
 const express = require("express");
 const App = express();
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const https = require("https");
-const { log } = require("console");
-App.set("view engine","ejs");
+
+App.set("view engine", "ejs");
 App.use(express.static("public"));
-App.use(bodyParser.urlencoded({extended:true}))
-App.get("/",function(req,res){
+App.use(bodyParser.urlencoded({ extended: true }));
 
-    res.sendFile(__dirname + "/index.html")
+App.get("/", function (req, res) {
+    const query = "Gilgit";
+    const url = "https://api.weatherapi.com/v1/current.json?key=b13d33af9f0346aca8b124004252602&q=" + query + "&aqi=no";
 
+    https.get(url, function (response) {
+        console.log(response.statusCode);
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data);
+            console.log(weatherData);
 
-})
-App.post("/home",function(req,res){
+            const temp = weatherData.current.temp_c;
+            console.log(temp);
+            const cond = weatherData.current.condition.text;
+            console.log(cond);
+            const icon = weatherData.current.condition.icon;
+            console.log(icon);
+            const place = weatherData.location.name;
+            console.log(place);
+            const humidity = weatherData.current.humidity; // Corrected
+            console.log(humidity);
+            const feelslike = weatherData.current.feelslike_c;
+            console.log(feelslike);
+            const visibility = weatherData.current.vis_km;
+            console.log(visibility);
+
+            res.render("index", { temp: temp, cond: cond, icon: icon, place: place, humidity: humidity, feelslike: feelslike, visibility: visibility });
+        });
+    }).on("error", function (e) {
+        console.error(`Got error: ${e.message}`);
+        res.send("Error fetching weather data.");
+    });
+});
+
+App.post("/home", function (req, res) {
     const query = req.body.Location;
+    const url = "https://api.weatherapi.com/v1/current.json?key=b13d33af9f0346aca8b124004252602&q=" + query + "&aqi=no";
 
-    const url = "https://api.weatherapi.com/v1/current.json?key=%20b13d33af9f0346aca8b124004252602&q="+query+"&aqi=no";
+    https.get(url, function (response) {
+        console.log(response.statusCode);
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data);
+            console.log(weatherData);
 
-https.get(url, function(response){
-    console.log(response.statusCode)
-response.on("data", function(data){
-    const weatherData = JSON.parse(data)
-    console.log(weatherData)
+            const temp = weatherData.current.temp_c;
+            console.log(temp);
+            const cond = weatherData.current.condition.text;
+            console.log(cond);
+            const icon = weatherData.current.condition.icon;
+            console.log(icon);
+            const place = weatherData.location.name;
+            console.log(place);
+            const humidity = weatherData.current.humidity; // Corrected
+            console.log(humidity);
+            const feelslike = weatherData.current.feelslike_c;
+            console.log(feelslike);
+            const visibility = weatherData.current.vis_km;
+            console.log(visibility);
 
-    const temp = weatherData.current.temp_c;
-     console.log(temp);
-     const cond = weatherData.current.condition.text;
-     console.log(cond);
-     const icon = weatherData.current.condition.icon;
-     console.log(icon);
-     const place = weatherData.location.name;
-     console.log(place)
+            res.render("result", { temp: temp, cond: cond, icon: icon, place: place, humidity: humidity, feelslike: feelslike, visibility: visibility });
+        });
+    }).on("error", function (e) {
+        console.error(`Got error: ${e.message}`);
+        res.send("Error fetching weather data.");
+    });
+});
 
-     res.render("result",{temp:temp,cond:cond,icon:icon,place:place})
-})
-
-
-
-    
-})
-
-
-
-    
-
-})
-
-App.get("/home",function(req,res){
-    res.render("home")
-})
-
-
+App.get("/home", function (req, res) {
+    res.render("home");
+});
 
 const port = process.env.PORT || 3000;
 
-App.listen(port, function(){
+App.listen(port, function () {
     console.log(`The Port Is ${port}`);
-})
+});
